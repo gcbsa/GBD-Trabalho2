1e946e0d7b8e12d5e8060257610146e039cce92c
ThwyIgo <thiagopachecorocha@hotmail.com>
2024-02-16 11:14:41 +0000

    Makefile

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    printf("%d\n", INT_MAX);
+    printf("%d\n", INT_MAX + 1);
+
+    return 0;
+}
